65d8a77950526ea9c81ea1c60db5a5e80736f8b4
itversity <dgadiraju@itversity.com>
2020-06-27 18:36:24 -0700

    Logging example

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import sys
+import sys, logging
 
 from util import get_tables, load_db_details
 from read import read_table
@@ -8,12 +8,18 @@ from write import load_table
 def main():
     env = sys.argv[1]
     a_tables = sys.argv[2]
+    logging.basicConfig(filename='data-copier.info',
+                        level=logging.INFO,
+                        format='%(asctime)s [%(message)s]',
+                        datefmt='%m/%d/%Y %I:%M:%S %p'
+                        )
+    logging.basicConfig(filename='data-copier.err', level=logging.ERROR)
     db_details = load_db_details(env)
     tables = get_tables('table_list', a_tables)
     for table_name in tables['table_name']:
-        print(f'reading data for {table_name}')
+        logging.info(f'reading data for {table_name}')
         data, column_names = read_table(db_details, table_name)
-        print(f'loading data for {table_name}')
+        logging.error(f'loading data for {table_name}')
         load_table(db_details, data, column_names, table_name)
 
 
